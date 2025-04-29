cfcf654d38245b222229aba594ea47f2cae27d11
alubeda <gua903@gmail.com>
2025-04-29 20:02:10 +0200

    Normalize body size regardless of the content

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import '@/styles/app.less'
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById('page-wrapper')).render(
   <StrictMode>
     <BrowserRouter>
       <App />
--- src/pages/ProductListPage.jsx
+++ src/pages/ProductListPage.jsx
@@ -106,5 +106,5 @@ export default function ProductListPage() {
                 </>
             }
         </div>
-    );
+    )
 }
